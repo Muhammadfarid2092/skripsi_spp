a4c81bfae212bdbbd0baf672f81d3f679e427ea1
Muhammadfarid2092 <muhfarid2092@gmail.com>
2022-12-16 20:37:29 +0700

    change colum name on table users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
-            $table->string('nik_nis');
+            $table->string('nip_nis');
             $table->string('password');
             $table->string('role');
             $table->char('kelas', 1)->nullable();
